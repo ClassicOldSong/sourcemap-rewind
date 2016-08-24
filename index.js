0481db3815ff8e7e6a8faaf21e98cc11e9d62b58
Septs <cao111210@gmail.com>
2016-08-24 22:03:21 +0800

    refactor

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 'use strict'
 const Promise = require('bluebird')
-const { promisify } = Promise
-const resolveSourceMap = promisify(require('source-map-resolve').resolveSourceMap)
-const resolveSources = promisify(require('source-map-resolve').resolveSources)
-const fs = require('fs')
+const {resolveSourceMapAsync, resolveSourcesAsync} = Promise.promisifyAll(require('source-map-resolve'))
+const fs = Promise.promisifyAll(require('fs'))
 const path = require('path')
+const mkdirp = Promise.promisify(require('mkdirp'))
 
 module.exports = (rawPath, outPath) => {
   const rawContent = fs.readFileSync(rawPath).toString()
 
-  return resolveSourceMap(rawContent, rawPath, fs.readFile)
-    .then(({map, sourcesRelativeTo}) => resolveSources(map, sourcesRelativeTo, fs.readFile))
+  return resolveSourceMapAsync(rawContent, rawPath, fs.readFile)
+    .then(({map, sourcesRelativeTo}) => resolveSourcesAsync(map, sourcesRelativeTo, fs.readFile))
     .then(({sourcesResolved, sourcesContent}) => sourcesResolved.map((filePath, index) => ({
       filePath: `${outPath}${path.resolve('/', filePath)}`.replace(':', ''),
       content: sourcesContent[index]
     })))
     .then(mapping => Promise.map(mapping, ({filePath, content}) => {
-      const mkdirp = promisify(require('mkdirp'))
-      const writeFile = promisify(fs.writeFile)
       return mkdirp(path.dirname(filePath))
-        .then(() => writeFile(filePath, content))
+        .then(() => fs.writeFileAsync(filePath, content))
+        .catch(() => Promise.reject(filePath))
+        .then(() => filePath)
     }))
 }
