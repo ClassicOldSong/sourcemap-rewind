4703bb118b728a9c98c4e5dccb5bc0662a299c9f
Septs <cao111210@gmail.com>
2016-08-24 21:19:30 +0800

    refactor

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,30 +1,42 @@
 #!/usr/bin/env node
+'use strict'
+const Promise = require('bluebird')
+const path = require('path')
 
 const argv = require('yargs')
-	.option('i', {
-		alias: 'input',
-		demand: true,
-		describe: 'Input file path',
-		type: 'string'
-	})
-	.option('o', {
-		alias: 'output',
-		demand: false,
-		default: './source',
-		describe: 'Output folder path',
-		type: 'string'
-	})
-	.usage('Usage: sourcemap-rewind [options]')
-	.example('sourcemap-rewind -i main.js -o ./output')
-	.help('h')
-	.alias('h', 'help')
-	.argv;
-
-const rewind = require('../');
+  .option('i', {
+    alias: 'input',
+    demand: true,
+    describe: 'Input file path',
+    type: 'string'
+  })
+  .option('o', {
+    alias: 'output',
+    demand: false,
+    default: './source',
+    describe: 'Output folder path',
+    type: 'string'
+  })
+  .usage('Usage: sourcemap-rewind [options]')
+  .example('sourcemap-rewind -i main.js -o ./output')
+  .help('h')
+  .alias('h', 'help')
+  .argv
 
 if (argv.i === '') {
-	console.log('No input file specified! Use "sourcemap-rewind -h" to display help info.');
-	process.exit(1);
+  console.log('No input file specified! Use "sourcemap-rewind -h" to display help info.')
+  process.exit(1)
 }
 
-rewind(argv.i, argv.o);
+require('../')(argv.i, argv.o)
+  .then(mapping => Promise.map(mapping, ({filePath, content}) => {
+    console.log(`Processing ${filePath}`)
+    const mkdirp = Promise.promisify(require('mkdirp'))
+    const writeFile = Promise.promisify(require('fs').writeFile)
+    return mkdirp(path.dirname(filePath))
+      .then(() => writeFile(filePath, content))
+  }))
+  .then(() => {
+    process.stdout.write('\r\x1b[K')
+    process.stdout.write(`Complete! You can find the resolved project at "${path.resolve(argv.o)}"`)
+  })
